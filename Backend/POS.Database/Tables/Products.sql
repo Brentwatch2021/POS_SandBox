9978e0378b16844fc3cb1253a727dd1f39b42862
Brent Becker <wirrem1990@gmail.com>
2026-05-23 17:51:05 +0200

    Added the DB stuff

---
+++ Backend/POS.Database/Tables/Products.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Products (
+    ProductId           INT                 IDENTITY (1,1)                                      PRIMARY KEY,
+
+    Barcode             VARCHAR(50)         NOT NULL CONSTRAINT DF_Products_Barcode             DEFAULT 'BC0000000',
+    ProductName         VARCHAR(200)        NOT NULL,
+
+    CurrentPrice        Money               NOT NULL CONSTRAINT DF_Products_CurrentPrice        DEFAULT 0,
+    VatRate             DECIMAL(5,2)        NOT NULL CONSTRAINT DF_Products_VatRate             DEFAULT 0,
+
+    IsProductActive     BIT                 NOT NULL CONSTRAINT DF_Products_IsProductActive     DEFAULT 1,
+    ProductCreatedAt    DATETIME            NOT NULL CONSTRAINT DF_Products_ProductCreatedAt    DEFAULT GETDATE()
+
+);
