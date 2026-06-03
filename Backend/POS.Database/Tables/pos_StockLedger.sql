5c82cffa8027cf8b6ef25cef7366ca7727644c7d
Brent Becker <wirrem1990@gmail.com>
2026-06-03 15:15:19 +0200

    [DB] Payment Tables for process of sale

--- Backend/POS.Database/Tables/pos_StockLedger.sql
+++ Backend/POS.Database/Tables/pos_StockLedger.sql
@@ -9,11 +9,9 @@ CREATE TABLE [dbo].[pos_StockLedger] (
                                         ELSE 'Unknown'
                                         END,
     [QuantityChange]                    INT NOT NULL,
-    [CartDetailId]                      BIGINT NOT NULL,
     [CreatedByUserId]                   INT,
     [StockLedgerCreatedAt]              DATETIME NOT NULL DEFAULT GETUTCDATE(),
     CONSTRAINT FK_pos_StockLedger_pos_Store FOREIGN KEY (StoreId) REFERENCES pos_StockMaster(StockMasterId),
-    CONSTRAINT FK_pos_StockLedger_pos_StockMaster FOREIGN KEY (StockMasterId) REFERENCES pos_StockMaster(StockMasterId),
-    CONSTRAINT FK_pos_StockLedger_pos_CartDetail  FOREIGN KEY (CartDetailId) REFERENCES pos_CartDetail(CartDetailId)
+    CONSTRAINT FK_pos_StockLedger_pos_StockMaster FOREIGN KEY (StockMasterId) REFERENCES pos_StockMaster(StockMasterId)
 );
 GO
--- Backend/POS.Database/Tables/pos_StockMaster.sql
+++ Backend/POS.Database/Tables/pos_StockMaster.sql
@@ -1,10 +1,12 @@
 CREATE TABLE [dbo].[pos_StockMaster] (
     [StockMasterId]           INT                 IDENTITY (1,1)                                      PRIMARY KEY,
     [StockMasterName]         VARCHAR(200)        NOT NULL,
+    [StoreId]                 INT                 NOT NULL,
     [CurrentPrice]            MONEY               NOT NULL CONSTRAINT DF_StockMaster_CurrentPrice            DEFAULT 0,
     [VatRate]                 DECIMAL(5,2)        NOT NULL CONSTRAINT DF_StockMaster_VatRate                 DEFAULT 0,
     [IsStockMasterActive]     BIT                 NOT NULL CONSTRAINT DF_StockMaster_IsStockMasterActive     DEFAULT 1,
     [StockMasterCreatedAt]    DATETIME            NOT NULL CONSTRAINT DF_StockMaster_StockMasterCreatedAt    DEFAULT GETUTCDATE()
+    CONSTRAINT FK_pos_StockMaster_pos_Store FOREIGN KEY (StoreId) REFERENCES pos_Store(StoreId)
 
 );
 GO
