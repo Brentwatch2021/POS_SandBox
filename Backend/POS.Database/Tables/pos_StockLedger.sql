de33cc50ad39df836d1d25b2ffba3eecc79ef6e7
Brent Becker <wirrem1990@gmail.com>
2026-06-03 15:35:51 +0200

    [DB] Payment Tables for process of sale

--- Backend/POS.Database/Tables/pos_StockLedger.sql
+++ Backend/POS.Database/Tables/pos_StockLedger.sql
@@ -9,7 +9,7 @@ CREATE TABLE [dbo].[pos_StockLedger] (
                                         ELSE 'Unknown'
                                         END,
     [QuantityChange]                    INT NOT NULL,
-    [CreatedByUserId]                   INT,
+    [CreatedByUserId]                   INT NOT NULL,
     [StockLedgerCreatedAt]              DATETIME NOT NULL DEFAULT GETUTCDATE(),
     CONSTRAINT FK_pos_StockLedger_pos_Store FOREIGN KEY (StoreId) REFERENCES pos_StockMaster(StockMasterId),
     CONSTRAINT FK_pos_StockLedger_pos_StockMaster FOREIGN KEY (StockMasterId) REFERENCES pos_StockMaster(StockMasterId)
